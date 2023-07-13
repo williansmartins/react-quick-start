2a79197c2b5173f8ca473033608608b7f585dc59
Willians Martins <professorwillians@gmail.com>
2023-07-13 00:27:10 +0000

    set href

--- project/next.config.js
+++ project/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: "build",
-    output: "export"
+    output: "export",
+    assetPrefix: "./"
 }
 
 module.exports = nextConfig
